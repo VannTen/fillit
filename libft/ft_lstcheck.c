1b08a707cbbab4770889fa1acf2835ea50bb65db
Max Gautier <mgautier@student.42.fr>
2016-11-23 14:37:55 +0100

    Comments and name change on ft_lstcheck

--- libft/ft_lstcheck.c
+++ libft/ft_lstcheck.c
@@ -6,11 +6,16 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/23 13:07:34 by mgautier          #+#    #+#             */
-/*   Updated: 2016/11/23 13:13:00 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/11/23 14:33:24 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/*
+** Check every element of a list with the given function and return the
+** number of elements who return TRUE
+*/
 
 unsigned int	ft_check_list(t_list list, t_bool (*f)(t_list *elem))
 {
