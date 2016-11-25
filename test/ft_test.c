2e2d40753b3da5fa1a4e816c107e8a311b012422
Max Gautier <mgautier@student.42.fr>
2016-11-25 17:43:58 +0100

    add temoin to the test

--- test/ft_test.c
+++ test/ft_test.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/24 11:28:07 by mgautier          #+#    #+#             */
-/*   Updated: 2016/11/24 13:27:11 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/11/25 17:42:56 by                  ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,5 @@ void	ft_test_reader(t_list *entry_list)
 void	ft_link_apply(t_list *elem)
 {
 	ft_putstr((char*)elem->content);
+	ft_putstr("LIMIT\n");
 }
