0e90b458324dc25cffdb39b2b742c6a098c1a9ca
Max Gautier <mgautier@student.42.fr>
2016-11-28 17:36:02 +0100

    Tests

--- test/maintest.c
+++ test/maintest.c
@@ -6,7 +6,7 @@
 /*   By: mgautier <mgautier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/24 11:40:05 by mgautier          #+#    #+#             */
-/*   Updated: 2016/11/24 13:20:45 by mgautier         ###   ########.fr       */
+/*   Updated: 2016/11/28 17:28:50 by mgautier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		return (EXIT_FAILURE);
 	entry = open_close_file(argv[1]);
-	ft_test_reader(entry);
-	return (EXIT_SUCCESS);
+	if (entry != NULL && ft_entry_is_valid(entry))
+	{
+		ft_test_reader(entry);
+		return (EXIT_SUCCESS);
+	}
+	else
+	{
+		ft_error(INVALID_ENTRY);
+		return (EXIT_FAILURE);
+	}
 }
